161126ff25e5c49d4e868bccc2b4179d9b0c590a
DESKTOP-QAAAD41\DeepBrain <andrii.karachevtsev@gmail.com>
2018-03-23 09:07:47 +0200

    image analitics app

---
+++ ImageAnalitics/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ImageAnalitics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(486, 420);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(504, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(430, 420);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(501, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Brightness Histogram:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(859, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 490);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ImageAnalitics";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ImageAnalitics/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImageAnalitics
+{
+    public partial class Form1 : Form
+    {
+        int[] hist;
+        private static SolidBrush histColor = new SolidBrush(Color.Black);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static int[] GetHistogramm(Bitmap image)
+        {
+            int[] result = new int[256];
+            for (int x = 0; x < image.Width; x++)
+                for (int y = 0; y < image.Height; y++)
+                {
+                    int i = (int)(255 * image.GetPixel(x, y).GetBrightness());
+                    result[i]++;
+                }
+
+            return result;
+        }
+
+
+        private static void DrawHistogramm(Graphics g, Rectangle rect, int[] hist)
+        {
+            float max = hist.Max();
+            if (max > 0)
+                for (int i = 0; i < hist.Length; i++)
+                {
+                    float h = rect.Height * hist[i] / (float)max;
+                    g.FillRectangle(histColor, i * rect.Width / (float)hist.Length, rect.Height - h, rect.Width / (float)hist.Length, h);
+                }
+        }
+
+        public static void EdgeDetection(Bitmap b, float threshold)
+        {
+            Bitmap bSrc = (Bitmap)b.Clone();
+
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData bmSrc = bSrc.LockBits(new Rectangle(0, 0, bSrc.Width, bSrc.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            int stride = bmData.Stride;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)bmData.Scan0;
+                byte* pSrc = (byte*)(void*)bmSrc.Scan0;
+
+                int nOffset = stride - b.Width * 3;
+                int nWidth = b.Width - 1;
+                int nHeight = b.Height - 1;
+
+                for (int y = 0; y < nHeight; ++y)
+                {
+                    for (int x = 0; x < nWidth; ++x)
+                    {
+                        var p0 = ToGray(pSrc);
+                        var p1 = ToGray(pSrc + 3);
+                        var p2 = ToGray(pSrc + 3 + stride);
+
+                        if (Math.Abs(p1 - p2) + Math.Abs(p1 - p0) > threshold)
+                            p[0] = p[1] = p[2] = 255;
+                        else
+                            p[0] = p[1] = p[2] = 0;
+
+                        p += 3;
+                        pSrc += 3;
+                    }
+                    p += nOffset;
+                    pSrc += nOffset;
+                }
+            }
+
+            b.UnlockBits(bmData);
+            bSrc.UnlockBits(bmSrc);
+        }
+
+        static unsafe float ToGray(byte* bgr)
+        {
+            return bgr[2] * 0.3f + bgr[1] * 0.59f + bgr[0] * 0.11f;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog { Filter = "Image file|*.jpg;*.png;*.bmp" };
+            Bitmap image;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                image = (Bitmap)Bitmap.FromFile(ofd.FileName);
+                EdgeDetection(image, 10);
+                this.pictureBox1.BackgroundImage = image;
+            };
+
+            image = (Bitmap)Image.FromFile(ofd.FileName);
+            hist = GetHistogramm(image as Bitmap);
+
+            this.pictureBox2.Paint += (o, g) => DrawHistogramm(g.Graphics, ClientRectangle, hist);
+            this.pictureBox2.Resize += (o, g) => Refresh();
+            Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                histColor = new SolidBrush(colorDialog1.Color);
+                this.pictureBox2.Paint += (o, g) => DrawHistogramm(g.Graphics, ClientRectangle, hist);
+                this.pictureBox2.Resize += (o, g) => Refresh();
+                Refresh();
+            }
+        }
+    }
+}
